d2728b626c58bf19bf722b7fc826bc461ea9b513
rajia sultana shraboni <shraboni3103@gmail.com>
2024-08-29 10:50:07 +0600

    introduction to javascript

---
+++ type.js
@@ -0,0 +1,21 @@
+//number type variable
+
+var age = 24;
+var price = 330;
+var weight = 68;
+console.log(age);
+console.log(typeof age);
+
+//string type variable
+var name = "shraboni";
+var address = "sultanpur"
+var country = 'bangladesh'
+console.log(country);
+console.log(typeof country)
+
+//boolean type variable
+
+var isRich = true;
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
