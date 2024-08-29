81931e64f20d8f3fd17f096cb9a57a42ba3d9e34
rajia sultana shraboni <shraboni3103@gmail.com>
2024-08-29 12:11:45 +0600

    convert string to integer and float

---
+++ convert.js
@@ -0,0 +1,26 @@
+var price = '20'
+var price = parseInt ('20');
+
+var orangePrice = '266'
+var orangePrice = parseInt (266);
+
+
+var applePrice = '2.5'
+var applePrice = parseFloat('2.5');
+
+//var total = applePrice + orangePrice;
+
+
+// console.log(price);
+// console.log(applePrice);
+// console.log(orangePrice);
+
+// console.log(total);
+
+var first = 0.1;
+var second = 0.2;
+
+var total = (first + second);
+console.log(total.toFixed(1));
+
+
---
+++ naming.js
@@ -0,0 +1,22 @@
+// variable anme kono keyword diye hobe na 
+var false = passed
+// variable e kono spacing diye use kora jabe na 
+var is rich = ""
+// varibale name kono number diye startt kora jabe na kintu shesh e number use kora jabe 
+var 3number = ""  
+var number3 = ""
+
+// variable name is case sensitive 
+address
+ADDRESS
+
+// how to write long variable name
+
+var my_current_home_address = "sultanpur"; //snake case
+var myCurrentHomeAddress = "sultanpur"; //camel case
+var MyCurrentHomeAddress = "sultanpur"; //pascal case
+var my current home address = "sultanpur";
+
+// reserved keywords
+
+
