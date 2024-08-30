a308b78495765773f8704eff6485f85f684a1057
rajia sultana shraboni <shraboni3103@gmail.com>
2024-08-31 02:01:24 +0600

    some codes are added here

---
+++ drinks.js
@@ -0,0 +1,18 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice = 550;
+
+if(burgerPrice > 500){
+    console.log("free coke");
+}
+else{
+    console.log("coke 30tk");
+}
+
+
+
---
+++ taskthree.js
@@ -0,0 +1,18 @@
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+// Sample Input:
+// 119
+
+// Sample Output:
+// 4
+
+var number = 119;
+remainder = number % 5 
+console.log(remainder)
+
